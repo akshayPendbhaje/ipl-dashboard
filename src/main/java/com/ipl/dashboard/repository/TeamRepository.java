c09e8cdc37f80b10c3e298ec09e37b5f0369eefd
Akshay P <akshay.pendbhaje@somaiya.edu>
2021-04-28 23:38:54 +0530

    initial commit code

---
+++ src/main/java/com/ipl/dashboard/repository/TeamRepository.java
@@ -0,0 +1,9 @@
+package com.ipl.dashboard.repository;
+
+import com.ipl.dashboard.model.Team;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeamRepository extends CrudRepository<Team,Long>{
+    Team findByTeamName(String teamName);    
+}
